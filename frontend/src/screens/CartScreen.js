26a937666dcd3b6ae6221a75dfb4080788750663
dawon <lyumilove9@gmail.com>
2021-04-14 00:55:09 +0900

    CartScreen 수정

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function CartScreen(props) {
     const productId = props.match.params.id;
-    const qty = props.lacation.search? Number(props.location.search.split('=')[1]) : 1;
+    const qty = props.location.search? Number(props.location.search.split('=')[1]) : 1;
     return (
         <div>
             <h1>Cart Screen</h1>
