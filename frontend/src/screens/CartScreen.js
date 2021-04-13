3e94e967ea38d072eb86d1e7d30efcf75baeb4a2
dawon <lyumilove9@gmail.com>
2021-04-14 00:47:41 +0900

    cartScreen 제작

---
+++ frontend/src/screens/CartScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function CartScreen(props) {
+    const productId = props.match.params.id;
+    const qty = props.lacation.search? Number(props.location.search.split('=')[1]) : 1;
+    return (
+        <div>
+            <h1>Cart Screen</h1>
+            <p>장바구니에 담기: 상품아이디: {productId} Qty: {qty} </p>
+        </div>
+    );
+}
