1e146536f975c74fecbbb9edb861fd74ea5d6efc
dawon <lyumilove9@gmail.com>
2021-04-13 23:24:33 +0900

    product screen페이지에 redux 적용

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -1,4 +1,4 @@
-import { PRODUCT_LIST_FAIL, PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS } from '../constants/productConstants';
+import { PRODUCT_DETAILS_FAIL, PRODUCT_DETAILS_REQUEST, PRODUCT_DETAILS_SUCCESS, PRODUCT_LIST_FAIL, PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS } from '../constants/productConstants';
 import Axios from 'axios';
 
 export const listProducts = () => async (dispatch) => {
@@ -10,5 +10,21 @@ export const listProducts = () => async (dispatch) => {
     dispatch({type: PRODUCT_LIST_SUCCESS, payload: data});
   } catch(error) {
     dispatch({type: PRODUCT_LIST_FAIL, payload: error.message});
+  }
+};
+
+export const detailsProduct = (productId) => async(dispatch) =>{
+  dispatch ({type: PRODUCT_DETAILS_REQUEST, payload: productId});
+  try{
+    const {data} = await Axios.get(`/api/products/${productId}`);
+    dispatch({type: PRODUCT_DETAILS_SUCCESS, payload: data});
+  } catch(error) {
+    dispatch({
+      type: PRODUCT_DETAILS_FAIL, 
+      payload: 
+      error.response && error.response.data.message 
+        ? error.response.data.message 
+        : error.message,
+    });
   }
 };
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,62 +1,79 @@
-import React from 'react';
-import data from '../data';
+import React, { useEffect } from 'react';
+import {useDispatch, useSelector} from 'react-redux';
 import Rating from '../components/Rating';
 import { Link } from 'react-router-dom';
+import { productDetailReducer } from '../reducers/productReducers';
+import MessageBox from '../components/MessageBox';
+import LoadingBox from '../components/LoadingBox';
+import { detailsProduct } from '../actions/productActions';
 
 export default function ProductScreen(props) {
-    const product = data.products.find((x) => x.id === props.match.params.id);
-    if(!product) {
-        return <div> Product Not Found </div>;
-    }
+    const dispatch = useDispatch();
+    const productId = props.match.params.id;
+    const productDetails = useSelector(state => state.productDetails);
+    const {loading, error, product} = productDetails;
+
+    useEffect(() => {
+        dispatch(detailsProduct(productId));
+    }, [dispatch, productId]);
     return  (
         <div>
+            {loading? (
+                <LoadingBox></LoadingBox>
+                ) : error ? ( 
+                <MessageBox variant="danger">{error}</MessageBox>
+                ) : (
+            <div>
             <Link to="/">이전 페이지로</Link>
             <div className="row top">
                 <div className="col-2">
                     <img className="large" src={product.image} alt={product.name}></img>
                 </div>
-                <div className="col-1">
-                    <ul>
-                        <li>
-                            <h1>{product.name}</h1>
-                        </li>
-                        <li>
-                        <Rating
-                            rating={product.rating}
-                            numReviews={product.numReviews}
-                        ></Rating>    
-                        </li>
-                        <li>Price : ${product.price}</li>
-                        <li>
-                            Description:
-                            <p>{product.description}</p>
-                        </li>
-                    </ul>
-                </div>
-                <div className="col-1">
-                    <div className="card card-body">
+                    <div className="col-1">
                         <ul>
                             <li>
-                                <div className="row">
-                                    <div>Price</div>
-                                    <div className="price">${product.price}</div>
-                                </div>
+                                <h1>{product.name}</h1>
                             </li>
                             <li>
-                                <div className="row">
-                                    <div>Status</div>
-                                    <div>{product.countInStock > 0 ? ( <span className="success">구매가능</span> ) : (
-                                    <span className="danger">품절</span>)}
-                                    </div>
-                                </div>
+                            <Rating
+                                rating={product.rating}
+                                numReviews={product.numReviews}
+                            ></Rating>    
                             </li>
+                            <li>Price : ${product.price}</li>
                             <li>
-                            <button className="primary block"> 장바구니에 담기</button>
+                                Description:
+                                <p>{product.description}</p>
                             </li>
                         </ul>
                     </div>
+                    <div className="col-1">
+                        <div className="card card-body">
+                            <ul>
+                                <li>
+                                    <div className="row">
+                                        <div>Price</div>
+                                        <div className="price">${product.price}</div>
+                                    </div>
+                                </li>
+                                <li>
+                                    <div className="row">
+                                        <div>Status</div>
+                                        <div>{product.countInStock > 0 ? ( <span className="success">구매가능</span> ) : (
+                                        <span className="danger">품절</span>)}
+                                        </div>
+                                    </div>
+                                </li>
+                                <li>
+                                <button className="primary block"> 장바구니에 담기</button>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
+    )}
+    </div>
+        
     )
 }
