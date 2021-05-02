fe6afc4af052d133f4b9cb4524c0e70771487b2c
dawon <lyumilove9@gmail.com>
2021-05-02 19:38:19 +0900

    배송주소 입력 페이지 제작

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -1,4 +1,4 @@
-import { CART_ADD_ITEM, CART_REMOVE_ITEM } from '../constants/cartConstants';
+import { CART_ADD_ITEM, CART_REMOVE_ITEM, CART_SAVE_SHIPPING_ADDRESS } from '../constants/cartConstants';
 import Axios from 'axios';
 
 export const addToCart = (productId, qty) => async(dispatch, getState) => {
@@ -22,4 +22,9 @@ export const removeFromCart = (productId) => (dispatch, getState) => {
     type: CART_REMOVE_ITEM, 
     payload: productId});
   localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems));
+};
+
+export const saveShippingAddress = (data) => (dispatch) => {
+  dispatch({type: CART_SAVE_SHIPPING_ADDRESS, payload: data});
+  localStorage.setItem('shippingAddress', JSON.stringify(data));
 };
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -11,7 +11,12 @@ const initialState = {
       : null,
   },
   cart: {
-    cartItems : localStorage.getItem('cartItems')? JSON.parse(localStorage.getItem('cartItems')):[],
+    cartItems : localStorage.getItem('cartItems')
+    ? JSON.parse(localStorage.getItem('cartItems'))
+    :[],
+    shippingAddress: localStorage.getItem('shippingAddress')
+    ? JSON.parse(localStorage.getItem('shippingAddress'))
+    : {},
   },
 };
 const reducer = combineReducers({
