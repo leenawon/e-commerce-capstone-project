0ad23280088f1a28a792aedeb6d183a8dbe35ee4
dawon <lyumilove9@gmail.com>
2021-05-06 23:49:50 +0900

    결제 내역 저장

--- backend/models/orderModel.js
+++ backend/models/orderModel.js
@@ -20,6 +20,7 @@ const orderSchema = new mongoose.Schema({
     address: {type: String, required: true}
   },
   paymentMethod: {type: String, required: true},
+  paymentResult: {id: String, status: String, update_time: String, email_address:String,},
   itemsPrice: {type: Number, required: true},
   shippingPrice: {type: Number, required: true},
   totalPrice: {type: Number, required: true},
