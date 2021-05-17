66fe0340151550b084170ea3e31b17aa1ac47e08
leenawon <nawon5154@gmail.com>
2021-05-17 21:10:54 +0900

    상품 수정 페이지 이미지 파일 업로드 및 수정 기능 추가

--- frontend/src/screens/ProductEditScreen.js
+++ frontend/src/screens/ProductEditScreen.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import Axios from 'axios';
 import { detailsProduct, updateProduct } from '../actions/productActions';
 import LoadingBox from '../components/LoadingBox';
 import MessageBox from '../components/MessageBox';
@@ -44,6 +45,32 @@ export default function ProductEditScreen(props) {
     //TODO: dispatch update product
     dispatch(updateProduct({_id: productId, name, price, image,category, brand, countInStock,description}));
   };
+
+  const [loadingUpload, setLoadingUpload] = useState(false);
+  const [errorUpload, setErrorUpload] = useState('');
+
+  const userSignin = useSelector((state) => state.userSignin);
+  const {userInfo} = userSignin;
+  const uploadFileHandler = async (e) => {
+    const file = e.target.files[0];
+    const bodyFormData = new FormData();
+    bodyFormData.append('image', file);
+    setLoadingUpload(true);
+    try {
+      const {data} = await Axios.post('/api/uploads', bodyFormData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+          Authorization: `Bearer ${userInfo.token}`
+        }
+      });
+      setImage(data);
+      setLoadingUpload(false);
+    } catch(error) {
+      setErrorUpload(error.message);
+      setLoadingUpload(false);
+    }
+  };
+
   return (
     <div>
       <form className="form" onSubmit={submitHandler}>
@@ -68,6 +95,12 @@ export default function ProductEditScreen(props) {
               <label htmlFor="image">상품 이미지</label>
               <input id="image" type="text" placeholder="상품 이미지를 입력하세요" value={image} onChange={(e) => setImage(e.target.value)}></input>
             </div> 
+            <div>
+              <label htmlFor="imageFile">상품 이미지 파일 </label>
+              <input id="imageFile" type="file" label="이미지를 골라주세요" onChange={uploadFileHandler}></input>
+              {loadingUpload && <LoadingBox></LoadingBox>}
+              {errorUpload && <MessageBox variant="danger">{errorUpload}</MessageBox>}
+            </div>
             <div>
               <label htmlFor="category">상품 카테고리</label>
               <input id="category" type="text" placeholder="상품 카테고리를 입력하세요" value={category} onChange={(e) => setCategory(e.target.value)}></input>
