3cfa858de1abf57a481fe6c7f7f4004942d4c4e4
leenawon <nawon5154@gmail.com>
2021-05-20 01:00:56 +0900

    결제 여부에 따른 배송요청 기능 버튼 추가

--- frontend/src/actions/orderActions.js
+++ frontend/src/actions/orderActions.js
@@ -1,6 +1,6 @@
 import Axios from 'axios';
 import { CART_EMPTY } from '../constants/cartConstants';
-import { ORDER_CREATE_FAIL, ORDER_CREATE_REQUEST, ORDER_CREATE_SUCCESS, ORDER_DETAILS_FAIL, ORDER_DETAILS_REQUEST, ORDER_DETAILS_SUCCESS, ORDER_PAY_SUCCESS, ORDER_PAY_REQUEST, ORDER_PAY_FAIL, ORDER_MINE_LIST_REQUEST, ORDER_MINE_LIST_FAIL, ORDER_MINE_LIST_SUCCESS,ORDER_LIST_REQUEST, ORDER_LIST_SUCCESS, ORDER_LIST_FAIL, ORDER_DELETE_REQUEST, ORDER_DELETE_SUCCESS, ORDER_DELETE_FAIL } from '../constants/orderConstants';
+import { ORDER_CREATE_FAIL, ORDER_CREATE_REQUEST, ORDER_CREATE_SUCCESS, ORDER_DETAILS_FAIL, ORDER_DETAILS_REQUEST, ORDER_DETAILS_SUCCESS, ORDER_PAY_SUCCESS, ORDER_PAY_REQUEST, ORDER_PAY_FAIL, ORDER_MINE_LIST_REQUEST, ORDER_MINE_LIST_FAIL, ORDER_MINE_LIST_SUCCESS,ORDER_LIST_REQUEST, ORDER_LIST_SUCCESS, ORDER_LIST_FAIL, ORDER_DELETE_REQUEST, ORDER_DELETE_SUCCESS, ORDER_DELETE_FAIL, ORDER_DELIVER_REQUEST, ORDER_DELIVER_SUCCESS, ORDER_DELIVER_FAIL } from '../constants/orderConstants';
 
 export const createOrder = (order) => async(dispatch, getState) => {
   dispatch({type: ORDER_CREATE_REQUEST, payload: order});
@@ -82,7 +82,7 @@ export const listOrders = () => async(dispatch, getState) => {
 };
 
 export const deleteOrder = (orderId) => async(dispatch, getState) => {
-  dispatch({type: ORDER_DETAILS_REQUEST, payload:orderId});
+  dispatch({type: ORDER_DELETE_REQUEST, payload:orderId});
   const {userSignin:{userInfo}} = getState();
   try {
     const {data} = Axios.delete(`/api/orders/${orderId}`, {
@@ -92,5 +92,23 @@ export const deleteOrder = (orderId) => async(dispatch, getState) => {
   } catch(error) {
     const message = error.response && error.response.data.message ? error.response.data.message : error.message;
     dispatch({type: ORDER_DELETE_FAIL, payload: message})
+  }
+};
+
+export const deliverOrder = (orderId) => async (dispatch, getState) => {
+  dispatch({type: ORDER_DELIVER_REQUEST , payload: orderId});
+  const {
+    userSignin: {userInfo},
+  } = getState();
+  try {
+    const {data} = Axios.put(`/api/orders/${orderId}/deliver`, {}, {
+      headers: {Authorization: `Bearer ${userInfo.token}`},
+    });
+    dispatch({type: ORDER_DELIVER_SUCCESS, payload: data});
+  } catch (error) {
+    const message = error.response && error.response.data.message 
+    ? error.response.data.message 
+    : error.message;
+    dispatch({type: ORDER_DELIVER_FAIL, payload: message});
   }
 };
