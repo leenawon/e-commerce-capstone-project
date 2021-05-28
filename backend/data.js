b8c355008754909686f95db6c2fdac04a9f103d1
dawon <lyumilove9@gmail.com>
2021-05-29 01:49:11 +0900

    data.js 수정

--- backend/data.js
+++ backend/data.js
@@ -17,39 +17,39 @@ const data = {
     },
   ],
   products: [
-    {
-      name: 'Denim Pants',
-      category: 'Pants',
-      image: '/images/product-1.jpg',
-      price: 20000,
-      countInStock: 10,
-      brand: 'Thinket',
-      rating: 5,
-      numReviews: 15,
-      description: 'high quality pants'
-    },
-    {
-      name: 'Flower Dress',
-      category: 'Dress',
-      image: '/images/product-2.jpg',
-      price: 25000,
-      countInStock: 20,
-      brand: 'Thinket',
-      rating: 4.5,
-      numReviews: 20,
-      description: 'high quality dress'
-    },
-    {
-      name: 'Flower Long Dress',
-      category: 'Dress',
-      image: '/images/product-2.jpg',
-      price: 28000,
-      countInStock: 0,
-      brand: 'Thinket',
-      rating: 4.0,
-      numReviews: 20,
-      description: 'high quality dress'
-    }
+    // {
+    //   name: 'Denim Pants',
+    //   category: 'Pants',
+    //   image: '/images/product-1.jpg',
+    //   price: 20000,
+    //   countInStock: 10,
+    //   brand: 'Thinket',
+    //   rating: 5,
+    //   numReviews: 15,
+    //   description: 'high quality pants'
+    // },
+    // {
+    //   name: 'Flower Dress',
+    //   category: 'Dress',
+    //   image: '/images/product-2.jpg',
+    //   price: 25000,
+    //   countInStock: 20,
+    //   brand: 'Thinket',
+    //   rating: 4.5,
+    //   numReviews: 20,
+    //   description: 'high quality dress'
+    // },
+    // {
+    //   name: 'Flower Long Dress',
+    //   category: 'Dress',
+    //   image: '/images/product-2.jpg',
+    //   price: 28000,
+    //   countInStock: 0,
+    //   brand: 'Thinket',
+    //   rating: 4.0,
+    //   numReviews: 20,
+    //   description: 'high quality dress'
+    // }
   ]
 };
 
