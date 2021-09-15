d7a7f919c4ac4ce467929d168cc01898fddcb2b3
leenawon <nawon5154@gmail.com>
2021-09-15 18:53:29 +0900

    reviewSchema 추가(productModel) 및 productRouter post reviews부분 추가

--- backend/routers/productRouter.js
+++ backend/routers/productRouter.js
@@ -87,4 +87,23 @@ productRouter.delete('/:id', isAuth, isAdmin, expressAsyncHandler(async (req, re
     }
 }));
 
+productRouter.post('/:id/reviews', isAuth, expressAsyncHandler(async(req,res) => {
+    const productId = req.params.id;
+    const product = await Product.findById(productId);
+    if(product) {
+        if(product.reviews.find((x) => x.name === req.user.name)) {
+            return res.status(400).send({message : '이미 리뷰를 작성하셨습니다.'});
+        }
+        const review = {name: req.user.name, comment: req.body.comment, rating: Number(req.body.rating)};
+        product.reviews.push(review);
+        product.numReviews = product.reviews.length;
+        product.rating = product.reviews.reduce((a,c) => c.rating + a, 0) /  product.reviews.length;
+        const updatedProduct = await product.save();
+        res.status(201).send({message:'리뷰가 작성되었습니다', review: updatedProduct.reviews[updatedProduct.reviews.length -1]});
+    } else {
+        res.status(404).send({message: 'Product Not Found'});
+    }
+})
+);
+
 export default productRouter;
