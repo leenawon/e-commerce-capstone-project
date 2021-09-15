7796ac98e62daa7a23e2431182d1d02727c361fc
leenawon <nawon5154@gmail.com>
2021-09-15 19:31:51 +0900

    productAction.js createReview 추가 & ProductScreen 리뷰 작성 기능 추가

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -16,7 +16,10 @@ import {
   PRODUCT_DELETE_SUCCESS,
   PRODUCT_CATEGORY_LIST_SUCCESS,
   PRODUCT_CATEGORY_LIST_REQUEST,
-  PRODUCT_CATEGORY_LIST_FAIL,} from '../constants/productConstants';
+  PRODUCT_CATEGORY_LIST_FAIL,
+  PRODUCT_REVIEW_CREATE_REQUEST,
+  PRODUCT_REVIEW_CREATE_SUCCESS,
+  PRODUCT_REVIEW_CREATE_FAIL,} from '../constants/productConstants';
 import Axios from 'axios';
 
 export const listProducts = ({seller= '', name='', category = '', }) => async (dispatch) => {
@@ -105,5 +108,20 @@ export const deleteProduct = (productId) => async(dispatch, getState) => {
     const message = 
       error.response && error.response.data.message ? error.response.data.message : error.message;
     dispatch({type: PRODUCT_DELETE_FAIL, payload: message});
+  }
+};
+
+export const createReview = (productId, review) => async(dispatch, getState) => {
+  dispatch({type: PRODUCT_REVIEW_CREATE_REQUEST});
+  const {userSignin: {userInfo}} = getState();
+  try {
+    const {data} = await Axios.post(`/api/products/${productId}/reviews`, review, {
+      headers: {Authorization: `Bearer ${userInfo.token}`}
+    });
+    dispatch({type: PRODUCT_REVIEW_CREATE_SUCCESS, payload: data.review});
+  } catch(error) {
+    const message = 
+      error.response && error.response.data.message ? error.response.data.message : error.message;
+    dispatch({type: PRODUCT_REVIEW_CREATE_FAIL, payload: message});
   }
 };
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -4,7 +4,8 @@ import Rating from '../components/Rating';
 import { Link } from 'react-router-dom';
 import MessageBox from '../components/MessageBox';
 import LoadingBox from '../components/LoadingBox';
-import { detailsProduct } from '../actions/productActions';
+import { createReview, detailsProduct } from '../actions/productActions';
+import { PRODUCT_REVIEW_CREATE_RESET } from '../constants/productConstants';
 
 export default function ProductScreen(props) {
     const dispatch = useDispatch();
@@ -12,14 +13,37 @@ export default function ProductScreen(props) {
     const [qty, setQty] = useState(1);
     const productDetails = useSelector((state) => state.productDetails);
     const {loading, error, product} = productDetails;
+    const userSignin = useSelector((state) => state.userSignin);
+    const { userInfo } = userSignin;
+
+    const productReviewCreate = useSelector((state) => state.productReviewCreate);
+    const {loading: loadingReviewCreate, error: errorReviewCreate, success: successReviewCreate} = productReviewCreate;
+
+    const [rating, setRating] = useState(0);
+    const [comment, setComment] = useState('');
 
     useEffect(() => {
+        if(successReviewCreate) {
+            window.alert("리뷰가 성공적으로 작성되었습니다");
+            setRating('');
+            setComment('');
+            dispatch({type: PRODUCT_REVIEW_CREATE_RESET});
+        }
         dispatch(detailsProduct(productId));
-    }, [dispatch, productId]);
+    }, [dispatch, productId, successReviewCreate]);
 
     const addToCartHandler = () => {
         props.history.push(`/cart/${productId}?qty=${qty}`);
     };
+
+    const submitHandler = (e) => {
+        e.preventDefault();
+        if(comment && rating) {
+            dispatch(createReview(productId, {rating, comment, name: userInfo.name}))
+        } else {
+            alert('별점과 리뷰를 작성해주세요');
+        }
+    }
 
     return  (
         <div>
@@ -107,6 +131,58 @@ export default function ProductScreen(props) {
                             
                         </div>
                     </div>
+                </div>
+                <div>
+                    <h2 id="reviews">Reviews</h2>
+                    {product.reviews.length === 0 && <MessageBox>작성된 리뷰가 없습니다.</MessageBox>}
+                    <ul>
+                        {product.reviews.map((review) => (
+                            <li key={review._id}>
+                                <strong>{review.name}</strong>
+                                <Rating rating={review.rating} caption=" "></Rating>
+                                <p>{review.createdAt.substring(0,10)}</p>
+                                <p>{review.comment}</p>
+                            </li>
+                        ))}
+                        <li>
+                            {userInfo ? (
+                                <form className="form" onSubmit={submitHandler}>
+                                    <div>
+                                        <h2>리뷰 작성</h2>
+                                    </div>
+                                    <div>
+                                        <label htmlFor="rating">별점</label>
+                                        <select id="rating" value={rating} onChange={(e) => setRating(e.target.value)}>
+                                            <option value="">선택하세요...</option>
+                                            <option value="1">1점</option>
+                                            <option value="2">2점</option>
+                                            <option value="3">3점</option>
+                                            <option value="4">4점</option>
+                                            <option value="5">5점</option>
+                                        </select>
+                                    </div>
+                                    <div>
+                                        <label htmlFor="comment">리뷰를 작성해주세요</label>
+                                        <textarea id="comment" value={comment} onChange={(e) => setComment(e.target.value)}></textarea>
+                                    </div>
+                                    <div>
+                                        <label/>
+                                        <button type="submit" className="primary">
+                                            작성하기
+                                        </button>
+                                    </div>
+                                    <div>
+                                        {loadingReviewCreate && <LoadingBox></LoadingBox>}
+                                        {errorReviewCreate && <MessageBox variant="danger">{errorReviewCreate}</MessageBox>}
+                                    </div>
+                                </form>
+                            ) : (
+                                <MessageBox>
+                                    리뷰를 작성하기 위해 <Link to="/signin">로그인</Link> 해주세요
+                                </MessageBox>
+                            )}
+                        </li>
+                    </ul>
                 </div>
             </div>
     )}
