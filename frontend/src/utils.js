ee8debb88949c73441096842093a24c68eced912
dawon <lyumilove9@gmail.com>
2021-09-22 20:28:33 +0900

    .

---
+++ frontend/src/utils.js
@@ -0,0 +1,41 @@
+export const prices = [
+  {
+    name: `모든 가격`,
+    min: 0,
+    max: 0,
+  },
+  {
+    name: `1만원 이하`,
+    min: 1,
+    max: 10000,
+  },
+  {
+    name: `1만원 - 5만원`,
+    min: 10000,
+    max: 50000,
+  },
+  {
+    name: `5만원 - 10만원`,
+    min: 50000,
+    max: 100000,
+  },
+];
+
+export const ratings = [
+  {
+    name: '4점 이상',
+    rating:4,
+  },
+  {
+    name: '3점 이상',
+    rating:3,
+  },
+  {
+    name: '2점 이상',
+    rating:2,
+  },
+  {
+    name: '1점 이상',
+    rating:1,
+  },
+];
