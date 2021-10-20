e0f8988c2ca00e58fa52c322ba5d9cb53947e76a
dawon <lyumilove9@gmail.com>
2021-10-20 17:58:53 +0900

    server.js 수정

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost:27017/thinket', {
+mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/thinket', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
